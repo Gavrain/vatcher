1e3bcc104ad55b1a98cb2302d30ad89a07bf9a7e
yes-star <sny666@email.swu.edu.cn>
2022-07-30 18:50:04 +0800

    服务端提交

---
+++ vatcher-service/app.js
@@ -0,0 +1,56 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+// const bodyparser = require('koa-bodyparser')
+const koaBody = require('koa-body');
+
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+//bodyparser不支持mult-form的换body
+// app.use(bodyparser({
+//   enableTypes:['json', 'form', 'text']
+// }))
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    // 上传存放的路劲
+    uploadDir: 'public/images/',
+    // 保持后缀名\
+    keepExtensions: true,
+  }
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
